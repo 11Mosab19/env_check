4ad77aef77991df9c008bb1c84fc22506fe3f817
11mosab19 <mosabmohammedhome10@gmail.com>
2026-05-09 15:43:11 +0300

    make sure that all keys has value and exist

--- checker.py
+++ checker.py
@@ -1,12 +1,32 @@
 import os
 from dotenv import load_dotenv , find_dotenv , dotenv_values
+
+
+
+
 #عرفت ازاي اجيب الداتا من الملف الحمدلله
 print("------------Example------------")
-ExampleData=dotenv_values(find_dotenv("example.env"))
-for k,v in ExampleData.items():
-    print(f"{k} => {v}")
-print("#"*20)
+ExampleData=dotenv_values(find_dotenv("example.env",raise_error_if_not_found=True))
+for k,v in ExampleData.items():#بتاكد ان كل كيي معاه فاليو 
+    if v == None:
+        raise Exception (f"Missing key value for {k}")
+    
+print("")
+
 print("------------Data------------")
-CurrentData=dotenv_values(find_dotenv(".env"))
-for k,v in CurrentData.items():
-    print(f"{k} => {v}")
+
+CurrentData=dotenv_values(find_dotenv(raise_error_if_not_found=True))
+for k,v in CurrentData.items():#بتاكد ان كل كيي معاه فاليو 
+    if v == None:
+        raise Exception (f"Missing key value{k}")
+    
+    
+    
+for ExD in ExampleData.keys():#بتاكد ان كل كيي مطلوب موجود في فايل ال env
+    if ExD not in CurrentData.keys():
+        raise Exception (f"The Key {ExD} Missing in your .env file ") 
+    
+for CrD in CurrentData.keys():#بتاكد ان مفيش كيي زياده مكتوب
+    if CrD not in ExampleData.keys():
+        raise Exception (f"the key {CrD} is not required")
+    
