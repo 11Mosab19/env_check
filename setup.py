3a8e225950f75444d07a36bc22f510fc930e7900
11mosab19 <mosabmohammedhome10@gmail.com>
2026-05-15 23:29:53 +0300

    to .exe file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages,setup
 
 setup(
     name="mEnv-Checker.py",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "python-dotenv",
